c705a52cd40eb6d8f260611c32542c2f855ed163
Christoph Cullmann <cullmann@kde.org>
2023-06-18 10:30:37 +0200

    add package reverse search

--- common.nix
+++ common.nix
@@ -437,6 +437,9 @@ in
     programs.direnv.enable = true;
     programs.direnv.nix-direnv.enable = true;
 
+    # reverse package search, https://github.com/nix-community/nix-index
+    programs.nix-index.enable = true;
+
     # generate the shell config
     programs.zsh = {
       enable = true;
