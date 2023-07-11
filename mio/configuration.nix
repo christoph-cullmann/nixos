a05dbb6d4178b2653e52e3a317350cafdb43d731
Christoph Cullmann <cullmann@kde.org>
2023-07-11 16:25:35 +0200

    update mio config

--- mio/configuration.nix
+++ mio/configuration.nix
@@ -11,7 +11,6 @@ in
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-  
 
       # manage persistent files
       "${impermanence}/nixos.nix"
@@ -20,10 +19,20 @@ in
       "${home-manager}/nixos"
   ];
 
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+  # use the latest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
 
   # use a high resolution
   boot.loader.systemd-boot.consoleMode = "max";
@@ -151,17 +160,28 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    alacritty
+    ark
     aspellDicts.de
     aspellDicts.en
     borgbackup
     btop
+    chromium
     clamav
+    clinfo
+    filelight
+    firefox
     gitFull
+    glxinfo
     hunspellDicts.de_DE
     hunspellDicts.en_US
+    libva-utils
     lsof
     mc
-    woeusb
+    unrar
+    unzip
+    vulkan-tools
+    wayland-utils
     zsh
     zsh-powerlevel10k
   ];
@@ -186,6 +206,9 @@ in
       noto-fonts
       noto-fonts-extra
       noto-fonts-emoji
+
+      # other nice mono spaced font
+      victor-mono
     ];
 
     # tune fontconfig
@@ -288,26 +311,18 @@ in
     hashedPassword = builtins.readFile "/data/nixos/password.secret";
   };
 
-
   home-manager.users.cullmann = { pkgs, ... }: {
     # initial version
     home.stateVersion = "22.11";
 
-    # sometimes doesn't work
-    manual.manpages.enable = false;
-
     # extra packages, stuff for work/kde/...
     home.packages = with pkgs; [
-      alacritty
-      ark
       calibre
-      chromium
       emacs
       falkon
       fdupes
       ffmpeg
       file
-      firefox
       gimp
       go
       hugo
@@ -315,6 +330,7 @@ in
       kate
       kcalc
       keychain
+      kompare
       konversation
       krita
       libjxl
@@ -334,8 +350,7 @@ in
       texlive.combined.scheme-small
       tigervnc
       tk
-      unrar
-      unzip
+      tokodon
       usbutils
       via
       vial
@@ -344,11 +359,12 @@ in
       xorg.xhost
     ];
 
-    # enable direnv integration
+    # https://github.com/nix-community/nix-direnv
     programs.direnv.enable = true;
+    programs.direnv.nix-direnv.enable = true;
 
-    # nix-shell on drugs
-    services.lorri.enable = true;
+    # reverse package search, https://github.com/nix-community/nix-index
+    programs.nix-index.enable = true;
 
     # generate the shell config
     programs.zsh = {
@@ -380,84 +396,9 @@ in
     };
   };
 
-
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
-  # };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
 }
 
