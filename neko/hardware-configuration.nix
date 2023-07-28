8ee8426f887c229ff537600b653d5a8d7f5bac99
Christoph Cullmann <cullmann@kde.org>
2023-07-28 17:36:50 +0200

    new install config

--- neko/hardware-configuration.nix
+++ neko/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" ];
   boot.initrd.kernelModules = [ "i915" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
@@ -20,32 +20,24 @@
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/D6ED-7B8C";
+    { device = "/dev/disk/by-uuid/F1C1-0271";
       fsType = "vfat";
     };
 
-  fileSystems."/boot-fallback" =
-    { device = "/dev/disk/by-uuid/D6ED-BC4A";
-      fsType = "vfat";
-    };
-
-  boot.initrd.luks.devices."crypt-disk1".device = "/dev/disk/by-uuid/d24c36a7-d39c-4de5-8fc0-33ff0262e964";
-  boot.initrd.luks.devices."crypt-disk1".allowDiscards = true;
-  boot.initrd.luks.devices."crypt-disk1".bypassWorkqueues = true;
-  boot.initrd.luks.devices."crypt-disk2".device = "/dev/disk/by-uuid/cde8caa0-be38-4ff8-a3b3-aa82bc587550";
-  boot.initrd.luks.devices."crypt-disk2".allowDiscards = true;
-  boot.initrd.luks.devices."crypt-disk2".bypassWorkqueues = true;
+  boot.initrd.luks.devices."crypt-system".device = "/dev/disk/by-uuid/2dc54953-958b-4c5a-8454-21c0b1d16222";
+  boot.initrd.luks.devices."crypt-system".allowDiscards = true;
+  boot.initrd.luks.devices."crypt-system".bypassWorkqueues = true;
 
   fileSystems."/nix" =
-    { device = "/dev/mapper/crypt-disk1";
+    { device = "/dev/mapper/crypt-system";
       fsType = "btrfs";
-      options = [ "device=/dev/mapper/crypt-disk2" "subvol=nix" "noatime" "compress=zstd" ];
+      options = [ "subvol=nix" "noatime" "compress=zstd" ];
     };
 
   fileSystems."/data" =
-    { device = "/dev/mapper/crypt-disk1";
+    { device = "/dev/mapper/crypt-system";
       fsType = "btrfs";
-      options = [ "device=/dev/mapper/crypt-disk2" "subvol=data" "noatime" "compress=zstd" ];
+      options = [ "subvol=data" "noatime" "compress=zstd" ];
     };
 
   fileSystems."/home" =
