d5e9da4ace22280ae57db448399eb7bbccab9128
Christoph Cullmann <cullmann@kde.org>
2024-02-27 23:32:59 +0100

    add tests

--- projects/kde/cleanup.sh
+++ projects/kde/cleanup.sh
@@ -9,6 +9,9 @@ rm -rf ~/projects/kde/src ~/projects/kde/build ~/projects/kde/usr || exit 1
 # get new kde-builder
 mkdir -p ~/projects/kde/src || exit 1
 git clone https://invent.kde.org/sdk/kde-builder.git ~/projects/kde/src/kde-builder || exit 1
+git clone https://invent.kde.org/sdk/kdesrc-build.git ~/projects/kde/src/kdesrc-build || exit 1
 
 # start from scratch
-exec ./src/kde-builder/kde-builder --rc-file=~/projects/kde/kdesrc-buildrc --include-dependencies breeze plasma-integration kwayland-integration konsole kate
+exec ./src/kde-builder/kde-builder --rc-file=~/projects/kde/kdesrc-buildrc  kguiaddons
+# exec ./src/kdesrc-build/kdesrc-build --rc-file=~/projects/kde/kdesrc-buildrc  kguiaddons
+# exec ./src/kde-builder/kde-builder --rc-file=~/projects/kde/kdesrc-buildrc --include-dependencies breeze plasma-integration kwayland-integration konsole kate
