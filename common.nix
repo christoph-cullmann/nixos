7d8c6fb1a37a1a7a655abab5951b1c556069459f
Christoph Cullmann <christoph@cullmann.io>
2024-06-25 21:36:54 +0200

    streamline config

--- common.nix
+++ common.nix
@@ -624,7 +624,9 @@ in
     };
 
     # https://github.com/nix-community/nix-direnv
-    programs.direnv.enable = true;
-    programs.direnv.nix-direnv.enable = true;
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
   };
 }
