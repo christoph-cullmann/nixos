63c89c1ef2da32459a49c920c9a41910fc9b1481
Christoph Cullmann <christoph@cullmann.io>
2024-09-05 17:48:49 +0200

    back to kdesrc-build

--- projects/kde/cleanup.sh
+++ projects/kde/cleanup.sh
@@ -6,9 +6,9 @@ cd ~/projects/kde || exit 1
 # kill all old stuff
 rm -rf ~/projects/kde/src ~/projects/kde/build ~/projects/kde/usr || exit 1
 
-# get new kde-builder
+# get new kdesrc-build
 mkdir -p ~/projects/kde/src || exit 1
-git clone https://invent.kde.org/sdk/kde-builder.git ~/projects/kde/src/kde-builder || exit 1
+git clone https://invent.kde.org/sdk/kdesrc-build.git ~/projects/kde/src/kdesrc-build || exit 1
 
 # start from scratch
-exec ./src/kde-builder/kde-builder --rc-file=~/projects/kde/kdesrc-buildrc --include-dependencies breeze konsole kate
+exec ./src/kdesrc-build/kdesrc-build --rc-file=~/projects/kde/kdesrc-buildrc --include-dependencies breeze konsole kate
