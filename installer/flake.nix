c465a55f7a06dc80a9db4f389d8848816ca15a80
Christoph Cullmann <christoph@cullmann.io>
2025-05-04 15:52:53 +0200

    try bcachefs again

--- installer/flake.nix
+++ installer/flake.nix
@@ -6,8 +6,10 @@
       exampleIso = nixos.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix"
           ({ config, pkgs, ... }: {
+            boot.supportedFilesystems = [ "bcachefs" ];
+            boot.kernelPackages = lib.mkOverride 0 pkgs.linuxPackages_latest;
             networking.wireless.enable = false;
             networking.networkmanager.enable = true;
           })
