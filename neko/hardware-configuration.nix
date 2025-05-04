4b25ac85e12246a716f0d7af26e1c5c8ce7a126a
Christoph Cullmann <christoph@cullmann.io>
2025-05-04 18:28:22 +0200

    use ids

--- neko/hardware-configuration.nix
+++ neko/hardware-configuration.nix
@@ -15,7 +15,7 @@
 
   # /boot efi partition to boot in UEFI mode
   fileSystems."/boot" = {
-    device = "UUID=78FD-8F29";
+    device = "/dev/disk/by-id/nvme-Seagate_FireCuda_530_ZP4000GM30013_7VS01VBM-part1";
     fsType = "vfat";
     neededForBoot = true;
     options = [ "fmask=0022" "dmask=0022" ];
@@ -23,7 +23,7 @@
 
   # /nix volume with the system & all persistent data
   fileSystems."/nix" = {
-    device = "UUID=df5e6f5c-9700-48fd-ab16-90b7a1b3bca2";
+    device = "/dev/disk/by-id/nvme-Seagate_FireCuda_530_ZP4000GM30013_7VS01VBM-part2:/dev/disk/by-id/nvme-CT2000P5PSSD8_213330E4ED05-part2:/dev/disk/by-id/nvme-Samsung_SSD_980_PRO_2TB_S69ENF0R846614L-part2";
     fsType = "bcachefs";
     neededForBoot = true;
   };
