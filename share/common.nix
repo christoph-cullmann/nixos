893f503b3df906315b0fa1c36feafaa962046483
Christoph Cullmann <christoph@cullmann.dev>
2025-11-22 11:06:58 +0100

    abcde is a fail, too

--- share/common.nix
+++ share/common.nix
@@ -369,7 +369,6 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    abcde
     alsa-utils
     aspellDicts.de
     aspellDicts.en
