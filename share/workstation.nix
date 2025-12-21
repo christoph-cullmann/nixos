5deb924ca2705b99aa3b4aa37c3a09f4d850d82d
Christoph Cullmann <christoph@cullmann.dev>
2025-12-21 17:47:21 +0100

    airplay random wakes my av receiver...

--- share/workstation.nix
+++ share/workstation.nix
@@ -32,27 +32,4 @@
     enable = true;
     openFirewall = true;
   };
-
-  #
-  # support AirPlay devices in local network
-  # https://wiki.nixos.org/wiki/PipeWire#AirPlay/RAOP_configuration
-  #
-
-  # avahi required for service discovery
-  services.avahi.enable = true;
-
-  services.pipewire = {
-    # opens UDP ports 6001-6002
-    raopOpenFirewall = true;
-
-    extraConfig.pipewire = {
-      "10-airplay" = {
-        "context.modules" = [
-          {
-            name = "libpipewire-module-raop-discover";
-          }
-        ];
-      };
-    };
-  };
 }
