845ee183638b345e833c8c592e869a0aa38a5386
Christoph Cullmann <christoph@cullmann.dev>
2026-03-26 19:47:00 +0100

    I don't need the large webengine

--- projects/kde/shell.nix
+++ projects/kde/shell.nix
@@ -163,7 +163,6 @@ export QT_QUICK_CONTROLS_STYLE_PATH=~/projects/kde/usr/lib/qml/QtQuick/Controls.
     qt6.qtvirtualkeyboard
     qt6.qtwayland
     qt6.qtwebchannel
-    qt6.qtwebengine
     qt6.qtwebsockets
     qt6.qtwebview
     qt6Packages.qgpgme
