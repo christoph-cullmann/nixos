1068605558206dcb22ed1dc63857fe02509376c7
Christoph Cullmann <christoph@cullmann.dev>
2026-04-01 11:44:54 +0200

    fix wireless

--- share/common.nix
+++ share/common.nix
@@ -94,13 +94,7 @@ in
   ];
 
   # use NetworkManager, if we have WiFi, allows Plasma to manage connections
-  # use iwd, only thing that works properly on e.g. Macs
   networking.networkmanager.enable = true;
-  networking.wireless.enable = pkgs.lib.mkForce false;
-  networking.wireless.iwd = {
-    enable = true;
-    settings.General.EnableNetworkConfiguration = true;
-  };
 
   # enable proper power management
   services.power-profiles-daemon.enable = false;
