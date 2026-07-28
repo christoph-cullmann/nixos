cfcc88b34b0e78ed5dcf762a41a8c8ccd1917df1
Christoph Cullmann <christoph@cullmann.dev>
2026-07-28 21:22:53 +0200

    no slimserver
    too often hangs on rescan

--- anko/configuration.nix
+++ anko/configuration.nix
@@ -26,29 +26,16 @@
   users.groups.media-files = {
   };
 
-  # Jellyfin media server, add it to media-files, too
+  # Jellyfin media server, add it to media-files & make it persistent
   services.jellyfin = {
     enable = true;
     openFirewall = true;
   };
   users.users.jellyfin.extraGroups = [ "media-files" ];
-
-  # Slim Server for Logitech Squeezebox Players, add it to media-files, too
-  services.slimserver = {
-    enable = true;
-  };
-  users.users.slimserver.extraGroups = [ "media-files" ];
-  networking.firewall.allowedTCPPorts = [ 3483 9000 9090 ];
-  networking.firewall.allowedUDPPorts = [ 3483 ];
-
-  # keep some stuff persistent for the services
   environment.persistence."/nix/persistent" = {
     directories = [
       # local Jellyfin instance
       { directory = "/var/lib/jellyfin"; mode = "0700"; user = "jellyfin"; }
-
-      # local Slim Server instance
-      { directory = "/var/lib/slimserver"; mode = "0700"; user = "slimserver"; }
     ];
   };
 }
