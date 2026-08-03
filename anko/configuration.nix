9d3d08f2d0ed8724e834c52669f4ff45efff192c
Christoph Cullmann <christoph@cullmann.dev>
2026-08-03 21:19:14 +0200

    fix config, define persistence stuff once

--- anko/configuration.nix
+++ anko/configuration.nix
@@ -32,24 +32,22 @@
     openFirewall = true;
   };
   users.users.jellyfin.extraGroups = [ "media-files" ];
-  environment.persistence."/nix/persistent" = {
-    directories = [
-      # local Jellyfin instance
-      { directory = "/var/lib/jellyfin"; mode = "0700"; user = "jellyfin"; }
-    ];
-  };
 
   # Slim Server for Logitech Squeezebox Players, add it to media-files & make it persistent
   services.slimserver = {
     enable = true;
   };
+  networking.firewall.allowedTCPPorts = [ 3483 9000 9090 ];
+  networking.firewall.allowedUDPPorts = [ 3483 ];
   users.users.slimserver.extraGroups = [ "media-files" ];
+
   environment.persistence."/nix/persistent" = {
     directories = [
+      # local Jellyfin instance
+      { directory = "/var/lib/jellyfin"; mode = "0700"; user = "jellyfin"; }
+
       # local Slim Server instance
       { directory = "/var/lib/slimserver"; mode = "0700"; user = "slimserver"; }
     ];
   };
-  networking.firewall.allowedTCPPorts = [ 3483 9000 9090 ];
-  networking.firewall.allowedUDPPorts = [ 3483 ];
 }
