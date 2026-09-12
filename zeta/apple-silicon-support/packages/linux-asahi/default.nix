7ea7fceb13b258a509603ab9c7060805f76b51df
Christoph Cullmann <christoph@cullmann.dev>
2026-09-12 16:00:22 +0200

    sync new state of apple silicon support

--- zeta/apple-silicon-support/packages/linux-asahi/default.nix
+++ zeta/apple-silicon-support/packages/linux-asahi/default.nix
@@ -26,15 +26,15 @@ let
         inherit stdenv lib;
 
         pname = "linux-asahi";
-        version = "7.1.8";
+        version = "7.1.13";
         modDirVersion = version;
         extraMeta.branch = "7.1";
 
         src = fetchFromGitHub {
           owner = "AsahiLinux";
           repo = "linux";
-          tag = "asahi-7.1.8-1";
-          hash = "sha256-hsRB5FJz3hRR0QakBY7+dRXD5p9z6ZQiuJZgghpaTjY=";
+          tag = "asahi-7.1.13-1";
+          hash = "sha256-G1MQyuqwVN4uwm7awpdAOwxZZ9UNETGRr6Z3sQCbzgY=";
         };
 
         kernelPatches = [
